2c8589ab93bbac9977cc09976ec34a3da01a897a
Hideki <Hideki@DESKTOP-NK3EA0N>
2021-09-12 16:24:06 -0300

    Correção na ordem de chegada

--- src/controller/ThreadSalto.java
+++ src/controller/ThreadSalto.java
@@ -2,10 +2,13 @@ package controller;
 
 public class ThreadSalto extends Thread {
 
-	public int distMax;
+	private int idSapo;
+	private int distMax = 50;
+	private double distPerc = 0;
+	private static int posicao = 0;
 
-	public ThreadSalto(int distMax) {
-		this.distMax = distMax;
+	public ThreadSalto(int idSapo) {
+		this.idSapo = idSapo;
 	}
 
 	@Override // Obrigatoriedade de executar método run()
@@ -14,17 +17,23 @@ public class ThreadSalto extends Thread {
 	}
 
 	private void pular() { // Método que realiza a movimentação dos sapos
-		double distPerc = 0; // Começar na posição 0
 
 		while (distPerc < distMax) {
 			// Gera números aleatórios entre 1 e 10
 			int pulo = (int) ((Math.random() * 10) + 1);
 			distPerc += pulo; // O número gerado é igual a distância do pulo
-			System.out.println("O sapo #" + getId() + " pulou " + pulo + "m." 
-			+ " e percorreu " + distPerc + "m.");
+			System.out.println("O sapo " + idSapo + " pulou " + pulo + "m" + " e já percorreu " + distPerc + "m");
+
+			try {
+				sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
 		}
-		System.out.println("\n=========> O sapo #" + getId() + " chegou <=========\n");
-		
+		posicao++;
+		System.out.println("\n=====> O sapo " + idSapo + " foi o " + posicao + "º a chegar <======\n");
+
 	}
 
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -5,19 +5,10 @@ import controller.ThreadSalto;
 public class Principal {
 
 	public static void main(String[] args) {
-		
-		int distMax = 50; // Distância máxima e limite da corrida, o sapo que chegar a 50m primeiro ganha
-		
-		Thread sapo1 = new ThreadSalto(distMax);
-		Thread sapo2 = new ThreadSalto(distMax);
-		Thread sapo3 = new ThreadSalto(distMax);
-		Thread sapo4 = new ThreadSalto(distMax);
-		Thread sapo5 = new ThreadSalto(distMax);
-		
-		sapo1.start();
-		sapo2.start();
-		sapo3.start();
-		sapo4.start();
-		sapo5.start();
+
+		for (int idSapo = 1; idSapo <= 5; idSapo++) {
+			Thread sapo = new ThreadSalto(idSapo);
+			sapo.start();
+		}
 	}
 }
